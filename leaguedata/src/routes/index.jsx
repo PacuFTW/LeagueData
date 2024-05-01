26acdc04b51c021e0ef978a34acd4189006afe10
Karakai Patrik <patrikkarakai@gmail.com>
2024-05-01 19:11:38 +0200

    Prepping receiveing images

--- leaguedata/src/routes/index.jsx
+++ leaguedata/src/routes/index.jsx
@@ -1,15 +1,52 @@
-export default function Index() {
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Index = () => {
+  const [selectedRank, setSelectedRank] = useState('');
+  const [userData, setUserData] = useState(null);
+  const ranks = ['Iron', 'Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Master', 'Grandmaster', 'Challenger'];
+
+  const handleChange = (event) => {
+    setSelectedRank(event.target.value);
+  };
+
+  const fetchData = async () => {
+    axios.get(`http://localhost:2024/api/champion-data/${selectedRank.toLowerCase()}`).then(response=>{
+      setUserData(response.data);
+    }).catch(e =>{
+      console.error(e);
+    });
+  };
+
+  const UserDataComponent = ({ userData }) => {
     return (
       <div>
-        <p className="mb-10">
-            Dimat elosztó oldal válaszd ki az óra anyagát és látni fogod az órán megoldott feladatokat lépésről lépésre levezetve.
-          </p>
-          <p>
-            Az elméleti segédlet az csak a legnehezebb fogalmak magyarázatát tartalmazza konyhanyelven, hogy könnyebb legyen megérteni.
-          </p>
-          <p>
-            Ha bármilyen kérdés van, akkor nyugodtan keressetek engem.
-          </p>
+        <p>randombullshit</p>
       </div>
     );
-  }
+  };
+
+  return (
+    <div className="h-screen">
+      <select
+        className="border border-gray-300 rounded p-2 mr-2"
+        value={selectedRank}
+        onChange={handleChange}
+      >
+        <option value="">Select Rank</option>
+        {ranks.map((rank, index) => (
+          <option key={index} value={rank}>{rank}</option>
+        ))}
+      </select>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={fetchData}
+      >
+        Get Data
+      </button>
+      {userData && <UserDataComponent userData={userData} />}
+    </div>
+  );
+};
+
+export default Index;
